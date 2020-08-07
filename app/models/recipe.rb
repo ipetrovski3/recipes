78c499833fe05a928d64dd3f7ebb4e1a57d08cb4
ipetrovski3 <ipetrovski3@gmail.com>
2020-08-07 12:14:05 +0200

    created model recipe with relations

---
+++ app/models/recipe.rb
@@ -0,0 +1,7 @@
+class Recipe < ApplicationRecord
+  belongs_to :user
+  has_many :ingredients, inverse_of: :recipe
+  has_many :instructions, inverse_of: :recipe
+  accepts_nested_attributes_for :ingredients, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :instructions
+end
