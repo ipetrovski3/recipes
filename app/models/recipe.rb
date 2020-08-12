12b8023c3cac1da0603c0629b5dfb0468fb778d2
ipetrovski3 <ipetrovski3@gmail.com>
2020-08-12 23:36:24 +0200

    added validations to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,4 +8,7 @@ class Recipe < ApplicationRecord
 
   accepts_nested_attributes_for :ingredients, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :instructions, allow_destroy: true, reject_if: :all_blank
+
+  validates :name, presence: true
+  validates :description, presence: true
 end
